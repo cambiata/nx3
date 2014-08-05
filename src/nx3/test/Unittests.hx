a19eb81bf10d9df12b4765203c7ccb61eb15f898
Jonas Nystrom <jonasnys@gmail.com>
2014-08-05 14:06:53 +0200

    Added TestNIterators

--- src/nx3/test/Unittests.hx
+++ src/nx3/test/Unittests.hx
@@ -12,18 +12,11 @@ class Unittests
 	{
 		var runner = new  TestRunner(); 
 		
-		var start_time : Date = Date.now();
-		//runner.add(new TestQ());
-		runner.add(new TestN());
-		//runner.add(new TestQuickSyntax());
-		runner.add(new TestTargetSvgXml());
-		//runner.add(new TestP());
-		//runner.add(new TestPBars());
-		runner.add(new TestEvents());
+		runner.add(new TestNIterators());
+		//runner.add(new TestN());
 		
-		var end_time: Date = Date.now();
 		var success = runner.run();		
-		TestRunner.print('Testing time: ' + Std.int(end_time.getTime() - start_time.getTime()) + 'ms');
+		
 	}
 	
 }
