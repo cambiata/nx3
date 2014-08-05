dd70dc8037f0bef8048bc01ca633e4d601f85c1e
Jonas Nystrom <jonasnys@gmail.com>
2014-08-05 13:56:54 +0200

    minor

--- Nx3Test/src/Main.hx
+++ Nx3Test/src/Main.hx
@@ -16,15 +16,6 @@ class Main
 	static public function main() 
 	{		
 		Unittests.performTests();
-		
-		/*
-		var float = 203.39999999999998;
-		var float2 = float.round2();
-		trace([float, float2]);
-		*/
-		//var s = new SvgComposer();
-		//s.addLine(10, 20, 30, 40);
-		//trace(s.getXml().toString());
 	
 	}
 }
