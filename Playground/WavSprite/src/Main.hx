75f6f8eceeb6ee090d0ee53d5427b7c8fc27be4a
Jonas Nystrom <jonasnys@gmail.com>
2014-11-04 08:46:22 +0100

    update

--- Playground/WavSprite/src/Main.hx
+++ Playground/WavSprite/src/Main.hx
@@ -24,24 +24,22 @@ class Main  extends Sprite
 	{
 		super();
 
-
 		var christ = WavTools.getIntsFromWavFile(openfl.Assets.getBytes("assets/christ.wavdata"))[0];
+	
+		var chri = WavTools.dspSubset(christ, 0, 60000);
+		var wsChri = new Wav16IntsSprite(chri, null, true);
+		this.addChild(wsChri);
 		
-		var ws = new Wav16IntsSprite(christ, null, true);
-		this.addChild(ws);	
-		
-		//var leftInts = ws.getInts()[0];		
-		//var ba:ByteArray = ByteArrayTools.fromBytes(WavTools.intsToMono16Bytes(leftInts));
-		
-		var intsSt = WavTools.dspSubset(christ, christ.length - 20000);
-		var wsSt = new Wav16IntsSprite(intsSt, null, true);
+		var st = WavTools.dspSubset(christ, christ.length - 20000);
+		var wsSt = new Wav16IntsSprite(st, null, true);
 		this.addChild(wsSt);
-		wsSt.x = 300;
+		wsSt.y = 300;
 		
-		var fyr = WavTools.getIntsFromWavFile( openfl.Assets.getBytes("assets/fyr.wavdata"))[0];
-		
+		//var st = WavTools.dspSubset(christ, 
+		//var fyr = WavTools.getIntsFromWavFile( openfl.Assets.getBytes("assets/fyr.wavdata"))[0];		
 		//var mix = WavTools.dspMix(christ, fyr, 20000);
-		var cross = WavTools.dspCrossfade(christ, fyr, 50000);
+		
+		var cross = WavTools.dspCrossfade(chri, st, 1000);
 		
 		var s:Sound = new Sound();		
 		s.loadPCMFromByteArray(WavTools.intsToMono16ByteArray(cross), cross.length, 'short', false);
