923ae0b68a481e7c260cd486af20ffef11f29f29
Jonas Nyström <jonasnys@gmail.com>
2014-11-10 13:29:11 +0100

    Removed RenderBase

---
+++ src/nx3/render/Renderer.hxx
@@ -0,0 +1,38 @@
+package nx3.render;
+import nx3.Constants;
+import nx3.EDirectionUDs;
+import nx3.ESign;
+import nx3.geom.BezieerTool;
+import nx3.geom.Rectangle;
+import nx3.EDirectionUD;
+import nx3.NBar;
+import nx3.NPart;
+import nx3.NVoice;
+import nx3.ENoteType;
+import nx3.PBar;
+import nx3.PColumnsDistancesCalculator;
+import nx3.PScore;
+import nx3.PSystem;
+import nx3.QNote.QNote16;
+import nx3.QNote.QNote2;
+import nx3.QNote.QNote4;
+import nx3.QNote.QNote8;
+import nx3.QVoice;
+import nx3.render.ITarget;
+import nx3.render.scaling.TScaling;
+import nx3.render.svg.SvgElements;
+import nx3.TPoint;
+import nx3.TPoints;
+using cx.ArrayTools;
+using nx3.ENoteValTools;
+
+/**
+ * ...
+ * @author Jonas Nyström
+ */
+
+class Renderer extends RendererBase
+{
+
+	
+}
